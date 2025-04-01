554a0563281888bb7d12f816f9a2d85e860b818e
ivan kruglov <juster050@yandex.com>
2025-04-01 11:28:18 +0300

    second version

--- src/main/java/your/package/Chair.java
+++
@@ -1,7 +0,0 @@
-public class Chair {
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "faculty_id")
-    private Faculty faculty;
-    
-    // getters and setters
-} 
--- src/main/java/your/package/Faculty.java
+++
@@ -1,8 +0,0 @@
-public class Faculty {
-    private String nameFaculty; // Make sure this field name matches
-
-    // Ensure you have the getter
-    public String getNameFaculty() {
-        return nameFaculty;
-    }
-} 
